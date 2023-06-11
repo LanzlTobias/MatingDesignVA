45cf9995bb68468e44832cee7d78f4044562c817
Tobias Lanzl <60113807+LanzlTobias@users.noreply.github.com>
2023-06-11 16:55:19 +0200

    Update Appendix_A.R

--- scripts/Appendix_A.R
+++ scripts/Appendix_A.R
@@ -1,22 +1,39 @@
-## Tests for the formulas in Appendix A.
-
 set.seed(1)
 library(ggplot2)
 library(dplyr)
 library(tidyr)
 library(moments)
 
-# Number of loci
+# Number of QTL
 l <- 10
 # Probability of the reference allele being positive
 p <- 0.5
 
-# Vector with the number of positive reference alleles
+# number of QTL with positive allele effect
+k <- l * p
+
+# Example for the calculation of Y
+x <- c(rep(-1, l - k), rep(1, k))
+
+table((x %o% x)[upper.tri(x %o% x)])
+
+# different sign
+k * (l - k)
+
+# same sign
+choose(l, 2) - k * (l - k)
+
+# Y
+choose(l, 2) - 2 * k * (l - k)
+
+
+# Vector with all possibilities for the number of positive reference alleles
 k_vec <- 0:l
 
 # P[K=k]
 choose(l, k_vec) * p ** k_vec * (1 - p) ** (l - k_vec)
 sum(choose(l, k_vec) * p ** k_vec * (1 - p) ** (l - k_vec))
+plot(k_vec, choose(l, k_vec) * p ** k_vec * (1 - p) ** (l - k_vec))
 
 # Vector of number of coupling phase linkages - repulsion phase linkages
 y_vec <- -choose(l, 2):choose(l, 2)
@@ -56,6 +73,14 @@ tibble(y_vec, tmp) %>%
   labs(caption = 'Probability density function of P[Y=y] for l=10. The red line shows -[l/2].') +
   scale_y_continuous(limit = c(0, 0.5))
 
+# Lower and upper limit for Y
+tibble(y_vec, tmp) %>%
+  filter(tmp > 0) %>%
+  summarize(lower_limit = min(y_vec),
+            upper_limit = max(y_vec))
+choose(l, 2)
+-l/2
+
 ## Data for fig.S6 with 1000 loci (additionally with p=0.75 and p=0.9)
 
 probability_function <- function(p, l) {
@@ -71,14 +96,11 @@ probability_function <- function(p, l) {
 
 l <- 1000
 p_df <-
-  bind_cols(lapply(c(0.5), probability_function, l = l))
+  bind_cols(lapply(c(0.5, 0.75, 0.9), probability_function, l = l))
 
-names(p_df) <- paste0('p=', c(0.5))
+names(p_df) <- paste0('p=', c(0.5, 0.75, 0.9))
 
 p_df$y <- -choose(l, 2):choose(l, 2)
 
 saveRDS(p_df, file = 'output/Appendix_A.RData')
-
-
-
 
